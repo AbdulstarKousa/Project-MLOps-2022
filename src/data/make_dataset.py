7f8f0481118b4251a1f84e0db85cb2627f1248cc
RasmusJuul <r.juul97@gmail.com>
2022-01-07 13:13:34 +0100

    create dataset and loader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
 import click
 import logging
+import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from datasets import load_dataset, Dataset
+from transformers import AutoTokenizer
 
+
+def load_data():
+    train_datasets = Dataset.from_pandas(pd.read_pickle("../../data/processed/train.pkl"))
+    test_datasets = Dataset.from_pandas(pd.read_pickle("../../data/processed/test.pkl"))
+    return train_datasets, test_datasets
+
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,6 +27,21 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    os.makedirs(input_filepath, exist_ok=True)
+    raw_datasets = load_dataset("imdb",cache_dir=input_filepath)
+
+    global tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    full_train_dataset = tokenized_datasets["train"]
+    full_test_dataset = tokenized_datasets["test"]
+
+    os.makedirs(output_filepath, exist_ok=True)
+    full_train_dataset.to_pandas().to_pickle(output_filepath+"/train.pkl")
+    full_test_dataset.to_pandas().to_pickle(output_filepath+"/test.pkl")
 
 
 if __name__ == '__main__':
