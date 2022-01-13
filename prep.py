761dd9ba4c58115bb258dff25c551d43a6537429
RasmusJuul <r.juul97@gmail.com>
2022-01-13 12:09:23 +0100

    fixed

--- prep.py
+++ prep.py
@@ -1,6 +1,6 @@
 import os
 
-os.makedirs("/data/raw", exist_ok=True)
-os.makedirs("/data/processed", exist_ok=True)
-os.makedirs("/models", exist_ok=True)
-os.makedirs("/reports/figures", exist_ok=True)
+os.makedirs("data/raw", exist_ok=True)
+os.makedirs("data/processed", exist_ok=True)
+os.makedirs("models", exist_ok=True)
+os.makedirs("reports/figures", exist_ok=True)
