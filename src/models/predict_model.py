bbb0e1092a148f9ccff2d7ace8d3de17efb76197
RasmusJuul <r.juul97@gmail.com>
2022-01-19 16:28:41 +0100

    ignore best model folder and prediction

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,21 +3,18 @@ import torch
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
                           pipeline)
 
-from src import _PATH_MODELS
+from src import (_PROJECT_ROOT, _PATH_MODELS)
 from src.data.make_dataset import load_data
 
 label_dict = {"LABEL_0": "NEGATIVE", "LABEL_1": "POSITIVE"}
 
-model = AutoModelForSequenceClassification.from_pretrained(
-    _PATH_MODELS + "/best_model", torchscript=True
-)
-tokenizer = AutoTokenizer.from_pretrained(_PATH_MODELS + "/best_model")
-
+model = AutoModelForSequenceClassification.from_pretrained(_PROJECT_ROOT+"/best_model",local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(_PROJECT_ROOT+"/best_model")
 classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-# classifier.save_pretrained(_PATH_MODELS+"/best_model")
 
 while True:
     query = input()
+    if query == "quit" or query == "exit":
+        break
     result = classifier(query)[0]
     print(label_dict[result["label"]] + " score: " + str(result["score"]))
