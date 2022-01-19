0c3aca45bf1b7f60a55ba95ef64d8a9a8c02a0fd
RasmusJuul <r.juul97@gmail.com>
2022-01-19 16:43:00 +0100

    predicting with exit options

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,15 +3,18 @@ import torch
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
                           pipeline)
 
-from src import (_PROJECT_ROOT, _PATH_MODELS)
+from src import _PATH_MODELS, _PROJECT_ROOT
 from src.data.make_dataset import load_data
 
 label_dict = {"LABEL_0": "NEGATIVE", "LABEL_1": "POSITIVE"}
 
-model = AutoModelForSequenceClassification.from_pretrained(_PROJECT_ROOT+"/best_model",local_files_only=True)
-tokenizer = AutoTokenizer.from_pretrained(_PROJECT_ROOT+"/best_model")
+model = AutoModelForSequenceClassification.from_pretrained(
+    _PROJECT_ROOT + "/best_model", local_files_only=True
+)
+tokenizer = AutoTokenizer.from_pretrained(_PROJECT_ROOT + "/best_model")
 classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
+print("To stop write quit or exit or ctrl-c")
 while True:
     query = input()
     if query == "quit" or query == "exit":
